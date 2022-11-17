405211d2a6a8a7813154559ddddb8ec9e06cb225
Efekurdoglu <efekurdoglu@outlook.com>
2022-11-17 22:22:58 +0300

    Updating latest

--- knn/src/main.cpp
+++ knn/src/main.cpp
@@ -8,8 +8,8 @@
 int main()
 {
     data_handler *dh = new data_handler();
-    dh->read_feature_vector("../dataset/train-images-idx3-ubyte");
-    dh->read_feature_labels("../dataset/train-labels-idx1-ubyte");
+    dh->read_feature_vector("./../dataset/train-images-idx3-ubyte");
+    dh->read_feature_labels("./../dataset/train-labels-idx1-ubyte");
     dh->split_data();
     dh->count_classes();
 
